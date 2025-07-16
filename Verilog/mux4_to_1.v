a14e5681a585823da9309adf8229ab11dd498cba
motta97 <155576275+motta97@users.noreply.github.com>
2025-07-16 19:53:39 +0300

    Update mux4_to_1.v
    added a stimulus

--- Verilog/mux4_to_1.v
+++ Verilog/mux4_to_1.v
@@ -22,3 +22,36 @@ or(out, y0, y1, y2, y3);
 
 
 endmodule
+module stiumuls_to_mux;
+reg IN0, IN1, IN2, IN3, S0, S1;
+wire OUT;
+
+
+
+mux4_to_1 my_mux(.in0(IN0), .in1(IN1), .in2(IN2), .in3(IN3), .s0(S0), .s1(S1), .out(OUT));
+
+
+initial begin
+IN0 = 1;
+IN1 = 0;
+IN2 = 1;
+IN3 = 0;
+
+S0= 0; S1=1;
+S0= 0;
+S1=1;
+//choose IN0
+  #1 $display ("S1 = %b, S0 = %b, OUTPUT  = %b",S1 ,S0,OUT);
+ // choose IN1 
+  S1 = 0; S0 = 1; 
+  #1 $display("S1 = %b, S0 = %b, OUTPUT = %b \n", S1, S0, OUTPUT); 
+  // choose IN2 
+  S1 = 1; S0 = 0; 
+  #1 $display("S1 = %b, S0 = %b, OUTPUT = %b \n", S1, S0, OUTPUT); 
+  // choose IN3 
+  S1 = 1; S0 = 1; 
+  #1 $display("S1 = %b, S0 = %b, OUTPUT = %b \n", S1, S0, OUTPUT); 
+
+end
+
+endmodule
